1fa7d20d1c7e45adfe299fab71c6d3a47d3f8bbc
Felipe Joglar <felipejoglar@gmail.com>
2018-02-24 12:32:20 +0100

    Implement Parcelable in Movie

--- app/src/main/java/com/fjoglar/popularmoviesapp/data/model/Movie.java
+++ app/src/main/java/com/fjoglar/popularmoviesapp/data/model/Movie.java
@@ -16,12 +16,16 @@
 
 package com.fjoglar.popularmoviesapp.data.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Movie {
+public class Movie implements Parcelable{
 
     @SerializedName("vote_count")
     @Expose
@@ -65,6 +69,9 @@ public class Movie {
     @SerializedName("release_date")
     @Expose
     private String releaseDate;
+
+    public Movie() {
+    }
 
     public int getVoteCount() {
         return voteCount;
@@ -121,4 +128,58 @@ public class Movie {
     public String getReleaseDate() {
         return releaseDate;
     }
+
+    // Parcelable implementation
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.voteCount);
+        dest.writeInt(this.id);
+        dest.writeByte(this.video ? (byte) 1 : (byte) 0);
+        dest.writeFloat(this.voteAverage);
+        dest.writeString(this.title);
+        dest.writeFloat(this.popularity);
+        dest.writeString(this.posterPath);
+        dest.writeString(this.originalLanguage);
+        dest.writeString(this.originalTitle);
+        dest.writeList(this.genreIds);
+        dest.writeString(this.backdropPath);
+        dest.writeByte(this.adult ? (byte) 1 : (byte) 0);
+        dest.writeString(this.overview);
+        dest.writeString(this.releaseDate);
+    }
+
+    protected Movie(Parcel in) {
+        this.voteCount = in.readInt();
+        this.id = in.readInt();
+        this.video = in.readByte() != 0;
+        this.voteAverage = in.readFloat();
+        this.title = in.readString();
+        this.popularity = in.readFloat();
+        this.posterPath = in.readString();
+        this.originalLanguage = in.readString();
+        this.originalTitle = in.readString();
+        this.genreIds = new ArrayList<Integer>();
+        in.readList(this.genreIds, Integer.class.getClassLoader());
+        this.backdropPath = in.readString();
+        this.adult = in.readByte() != 0;
+        this.overview = in.readString();
+        this.releaseDate = in.readString();
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
