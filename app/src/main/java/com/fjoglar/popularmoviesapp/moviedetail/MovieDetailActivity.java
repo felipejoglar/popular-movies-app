5a8a6be0e99a5920ef149d2791968f136025854d
Felipe Joglar <felipejoglar@gmail.com>
2018-03-03 17:52:10 +0100

    Create Movies Detail layout

--- app/src/main/java/com/fjoglar/popularmoviesapp/moviedetail/MovieDetailActivity.java
+++ app/src/main/java/com/fjoglar/popularmoviesapp/moviedetail/MovieDetailActivity.java
@@ -21,7 +21,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.fjoglar.popularmoviesapp.R;
@@ -33,6 +35,7 @@ import com.fjoglar.popularmoviesapp.movies.MoviesAdapter;
 import com.fjoglar.popularmoviesapp.movies.MoviesContract;
 import com.fjoglar.popularmoviesapp.movies.MoviesPresenter;
 import com.fjoglar.popularmoviesapp.util.schedulers.SchedulerProvider;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -43,7 +46,14 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
     private MovieDetailContract.Presenter mMovieDetailPresenter;
 
     private ProgressBar mProgressLoading;
-    private TextView mTextMovie;
+    private ImageView mImgCover;
+    private ImageView mImgPoster;
+    private TextView mTextTitle;
+    private TextView mTextReleaseDate;
+    private TextView mTextRatingScore;
+    private TextView mTextTotalVotes;
+    private RatingBar mRatingScore;
+    private TextView mTextSynopsis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +61,14 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
         setContentView(R.layout.activity_movie_detail);
 
         mProgressLoading = findViewById(R.id.progress_loading);
-        mTextMovie = findViewById(R.id.text_movie);
+        mImgCover = findViewById(R.id.img_cover);
+        mImgPoster = findViewById(R.id.img_poster);
+        mTextTitle = findViewById(R.id.text_title);
+        mTextReleaseDate = findViewById(R.id.text_release_date);
+        mTextRatingScore = findViewById(R.id.text_rating_score);
+        mTextTotalVotes = findViewById(R.id.text_total_votes);
+        mRatingScore = findViewById(R.id.rating_score);
+        mTextSynopsis = findViewById(R.id.text_synopsis);
 
         mMovieDetailPresenter = new MovieDetailPresenter(this,
                 getIntent().getParcelableExtra(INTENT_EXTRA_MOVIE));
@@ -91,7 +108,20 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
 
     @Override
     public void showMovie(Movie movie) {
-        mTextMovie.setText(movie.getTitle());
+        Picasso.with(this)
+                .load("http://image.tmdb.org/t/p/w1280" +
+                        movie.getBackdropPath())
+                .into(mImgCover);
+        Picasso.with(this)
+                .load("http://image.tmdb.org/t/p/w500" +
+                        movie.getPosterPath())
+                .into(mImgPoster);
+        mTextTitle.setText(movie.getTitle());
+        mTextReleaseDate.setText(movie.getReleaseDate());
+        mTextRatingScore.setText(String.valueOf(movie.getVoteAverage()));
+        mTextTotalVotes.setText(String.valueOf(movie.getVoteCount()));
+        mRatingScore.setRating(movie.getVoteAverage() / 2);
+        mTextSynopsis.setText(movie.getOverview());
     }
 
     @Override
