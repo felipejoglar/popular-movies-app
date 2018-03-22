e979d80c2222b6f78b883072623c56fe2ed5e8ba
Felipe Joglar <felipejoglar@gmail.com>
2018-03-22 17:36:50 +0100

    Fix issue with application theme
    Put splash screen in application theme instead of
    in activity theme. Fixed now.

--- app/src/main/java/com/fjoglar/popularmoviesapp/moviedetail/MovieDetailActivity.java
+++ app/src/main/java/com/fjoglar/popularmoviesapp/moviedetail/MovieDetailActivity.java
@@ -92,7 +92,6 @@ public class MovieDetailActivity extends AppCompatActivity implements MovieDetai
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
 
